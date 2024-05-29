bd17e7c24b1db0f956c2999da13a4fcea0e413a0
abondiaye~ <ndiayeaboulaye657@gmail.com>
2024-05-29 20:06:21 +0200

    initial commit

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class CreateAdminCommand extends Command
+{
+    protected static $defaultName = 'app:create-admin';
+    private $entityManager;
+    private $passwordHasher;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Create an admin user')
+            ->setHelp('This command allows you to create an admin user...');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordHasher->hashPassword(
+            $user,
+            'adminpassword'
+        ));
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $io->success('Admin user created successfully.');
+
+        return Command::SUCCESS;
+    }
+}
+
+
